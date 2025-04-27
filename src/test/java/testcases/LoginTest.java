b53b36e78da8224822760fd4dc4914a637819612
Mihir Panda <mhrpanda13@gmail.com>
2025-04-27 13:02:20 +0530

    1st git bash changes

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -17,15 +17,15 @@ public class LoginTest extends BaseTest {
 	        loginPage.enterPassword();
 	        loginPage.clickLogin();
 	        
-	     // Capture and print the title
+	        // Capture and print the title
 	        String actualTitle = driver.getTitle();
 	        System.out.println("Page title after login: " + actualTitle);
-
 	        // Expected title (change it based on what your app shows)
 	        String expectedTitle = "OrangeHRM";
-
 	        // Assertion to verify title
 	        Assert.assertEquals(actualTitle, expectedTitle, "Login failed or unexpected page title.");
+	        
+	      //Changes for test
 	    }
 }
 
